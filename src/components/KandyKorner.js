5f91cbc68ba6e1ee2818f3c59da2f13896ebe57b
durrjp <durrjp1@gmail.com>
2020-04-24 14:13:10 -0500

    merging branch to another

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -4,16 +4,6 @@ import { Route, Redirect } from "react-router-dom"
 import Login from "./auth/Login"
 import Register from "./auth/Register"
 
-
-// export default () => {
-//     const [check, update] = useState(false)
-//     const toggle = () => update(!check)
-
-//     return (
-//         localStorage.getItem("kandy_customer") ? <Dashboard toggle={toggle} /> : <Auth toggle={toggle} />
-//     )
-// }
-
 export default () => (
     <>
         <Route render={() => {
